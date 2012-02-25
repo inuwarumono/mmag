32d1824eb41da64ccfb8bf326c777793c3465454
inuwarumono <inu@warumono.jp>
2012-02-25 11:08:26 +0900

    form

--- js/mmag.js
+++ js/mmag.js
@@ -3,5 +3,13 @@
 */
 
 $().ready(function() {
+  $('#form').submit(function(){
+    $('#main').children('a').fadeOut(function(){
+      $('#main').html('');
+    });
+  var query = $(this).children('input=["name"]').val();
+  if (!$query) return false;
+  $('#main').text('loading...');
+
   console.log('document is ready');
 });
